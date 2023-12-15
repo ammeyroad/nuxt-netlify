c81ec9fa5ab30c421f0fd853a0bd4e64cd6af00e
ammeyroad <69845507+ammeyroad@users.noreply.github.com>
2023-12-15 13:59:32 +0700

    Update _rups.vue

--- pages/rups/_rups.vue
+++ pages/rups/_rups.vue
@@ -6,12 +6,7 @@
       </nav>
 
       <article>
-        <img
-          v-if="post.gambar"
-          class="cover-image"
-          :src="post.gambar"
-        >
-        <!-- <h6 class="inline py-1 px-2 mr-1 bg-gray text-white text-sm font-medium rounded-sm">{{ post.kategori }}</h6> -->
+     
         <h1 class="">{{ post.judul }}</h1>
         <p class="mt-1 mb-8 text-primary-600 dark:text-primary-400">{{ post.deskripsi }}</p>
       </article>
